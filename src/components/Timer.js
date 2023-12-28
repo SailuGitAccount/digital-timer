45d0ca7b3d999242c1bd33e0d7493a7f15047902
Satya <msplendid16@gmail.com>
2023-12-28 16:40:19 -0600

    added code

---
+++ src/components/Timer.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+const Timer1 = () => {
+  // state to store time
+  const [time, setTime] = useState(0);
+
+  // state to check stopwatch running or not
+  const [ticking, setTicking] = useState(false);
+  const title="Stop Watch"
+
+  useEffect(() => {
+    let intervalId;
+    if (ticking) {
+      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [ticking, time]);
+
+  // Hours calculation
+  const hours = Math.floor(time / 360000);
+
+  // Minutes calculation
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  // Seconds calculation
+  const seconds = Math.floor((time % 6000) / 100);
+
+  // Milliseconds calculation
+  const milliseconds = time % 100;
+
+  // Method to start and stop timer
+  const startAndStop = () => {
+    setTicking(!ticking);
+  };
+
+  // Method to reset timer back to 0
+  const reset = () => {
+    setTime(0);
+  };
+  return (<>
+    <Grid
+              item
+              key={title}
+              xs={8}
+              sm={ 8 }
+              md={4}
+              style={{textAlign: "center"}}
+            >
+              <Card>
+                <CardHeader
+                  title={title}
+                  titleTypographyProps={{ align: 'center',fontSize:'32px'}}
+                  subheaderTypographyProps={{
+                    align: 'center',
+                  }}
+                  sx={{
+                    backgroundColor: (theme) =>
+                      theme.palette.mode === 'light'
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                  }}
+                />
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'baseline',
+                      mb: 2,
+                    }}
+                  >
+                    
+                    <Typography variant="h2" color="text.secondary">
+                    {hours}:{minutes.toString().padStart(2, "0")}:
+                    {seconds.toString().padStart(2, "0")}
+                    :{milliseconds.toString().padStart(2, "0")}
+                    </Typography>
+                  </Box>
+                  
+                </CardContent>
+                <CardActions>
+                  
+                  {/* <Box  sx={{
+                    textAlign:'center',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'baseline',
+                      mb: 2,
+                    }}> */}
+                    
+<Box textAlign='center' style={{marginLeft:'auto',marginRight:'auto'}}>
+        <Button variant="contained" onClick={startAndStop} style={{marginRight:'15px'}}>
+          {ticking ? "Stop" : "Start"} 
+        </Button>
+        <Button variant="contained" onClick={reset} >
+          Reset
+        </Button>
+      </Box>
+                </CardActions>
+              </Card>
+            </Grid>
+
+    </>
+  );
+};
+
+export default Timer1;
